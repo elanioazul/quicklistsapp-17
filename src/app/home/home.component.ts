e3748150558ce860d3d6fba62867f435b6738555
hugo.herrador <hugo.herrador@inetum.com>
2023-11-23 16:35:31 +0100

    create form modal comp

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { ModalComponent } from '../shared/ui/modal/modal.component';
+import { FormModalComponent } from '../shared/ui/form-modal/form-modal.component';
 import { Checklist } from '../shared/interfaces/checklist';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +13,37 @@ import { Checklist } from '../shared/interfaces/checklist';
       <button (click)="checklistBeingEdited.set({})">Add Checklist</button>
     </header>
     <app-modal [isOpen]="!!checklistBeingEdited()">
-      <ng-template> You can't see me... yet </ng-template>
+      <ng-template>
+          <app-form-modal
+            [title]="
+              checklistBeingEdited()?.title
+                ? checklistBeingEdited()!.title!
+                : 'Add Checklist'
+            "
+            [formGroup]="checklistForm"
+            (close)="checklistBeingEdited.set(null)"
+          />
+      </ng-template>
     </app-modal>
   `,
   styles: ``,
-  imports: [ModalComponent]
+  imports: [ModalComponent, FormModalComponent]
 })
 export default class HomeComponent {
+  formBuilder = inject(FormBuilder);
   checklistBeingEdited = signal<Partial<Checklist> | null>(null);
+  checklistForm = this.formBuilder.nonNullable.group({
+    title: [''],
+  });
+
+  constructor() {
+    effect(() => {
+      const checklist = this.checklistBeingEdited();
+
+      if (!checklist) {
+        this.checklistForm.reset();
+      }
+    })
+  }
 
 }
